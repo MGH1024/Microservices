03a00fbd2967dc3773d1c08df5f8ddd8fe9e744c
Meysam <Meysam@MGH>
2023-05-12 23:09:45 +0330

    add appDbContext to Product api project and add migration

---
+++ Services/Services.ProductApi/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Services.ProductApi.Models;
+
+namespace Services.ProductApi.DbContexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
